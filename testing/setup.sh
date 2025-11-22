c3576f6a94aeecbf95af06cba802df1f70827d74
Lenard Frommelt <lenny09918050@gmail.com>
2025-11-22 20:01:16 +0300

    thingysync push

--- testing/setup.sh
+++ testing/setup.sh
@@ -4,6 +4,7 @@
 #region removeStuff
 systemctl stop tested.socket
 systemctl stop tested.service
+systemctl stop tested.path
 rm /run/tested.sk
 #endregion
 
@@ -11,12 +12,15 @@ rm /run/tested.sk
 #region copyStuff
 cp tested.service /etc/systemd/system/
 cp tested.socket /etc/systemd/system/
+cp tested.path /etc/systemd/system/
+cp restart-tested.service /etc/systemd/system/
 cp nginx-config /etc/nginx/servers/tested
 #endregion
 
 ############################################################
-#region reloadAndRestart
+#region reloadAnd(Re)start
 systemctl daemon-reload
 systemctl start tested.socket
+systemctl start tested.path
 nginx -s reload
 #endregion
